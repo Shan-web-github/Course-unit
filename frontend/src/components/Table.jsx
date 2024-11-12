ba7785ef7bc5547e7868305af60642a5ab820318
Madushan <madushans18929@gmail.com>
2024-11-12 15:45:01 +0530

    backend

---
+++ frontend/src/components/Table.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Table({ columns, rows }) {
+    const data = Object.values(rows).map(value => `Value: ${value}`);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {columns.map((col, index) => (
+              <th key={index}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {columns.map((col, colIndex) => (
+                <td key={colIndex}>{row[col]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
